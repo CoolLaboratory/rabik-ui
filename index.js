d17b88e73fe77dbd567bf8b0bd6a9df3fa18b5de
Angelos Koulouris <cool.lab.gr@gmail.com>
2023-12-30 11:53:52 +0200

    update exports in index.js

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 export { default as MyCounterButton } from './components/MyCounterButton.svelte';
 export { default as Other } from './components/Other.svelte';
+
+export { default as Avatar } from './components/Avatar.svelte';
+export { default as IconButton } from './components/IconButton.svelte';
+export { default as DestinationLink } from './components/DestinationLink.svelte';
